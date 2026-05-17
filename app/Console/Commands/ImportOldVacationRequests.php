8d487b6b19887c17bca8223799c3007c4157de7c
cocenage <89825630221@mail.ru>
2026-05-17 22:43:03 +0500

    fix

---
+++ app/Console/Commands/ImportOldVacationRequests.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ImportOldVacationRequests extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-old-vacation-requests';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
